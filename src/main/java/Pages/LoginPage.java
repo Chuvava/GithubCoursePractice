18a4148646c7b092875c3f1a321c11394fc216a6
Aleksandr Efimov <aleksandr.efimov94@mail.ru>
2023-03-03 13:54:52 +0100

    added/updated/deleted file

--- src/main/java/Pages/LoginPage.java
+++ src/main/java/Pages/LoginPage.java
@@ -3,6 +3,10 @@ package Pages;
 public class LoginPage {
 
     public static void main(String a[]) {
+
         System.out.println("Hello LP");
+        String un = "admin";
+        String pwd = "admin";
+        System.out.println("logged in");
     }
 }
